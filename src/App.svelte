879e0aa6ed970e03fb59f3cbcb543cfa85c4771f
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-08-16 12:54:07 -0400

    add page title and description

--- src/App.svelte
+++ src/App.svelte
@@ -12,6 +12,16 @@
   let colors = ["#ea4f19", "#b15222", "#75542a", "#3c5733", "#005739"];
 </script>
 
+<section>
+  <h1>
+    Brewed Maps: A Choropleth Blend of Dunkin' & Starbucks Across the States
+  </h1>
+  <p>
+    Sip your way through the U.S. with this D3.js choropleth map, showcasing the
+    ratio of Dunkin' Donuts to Starbucks locations. A tasteful blend of data
+    visualization and coffee culture, served in a Svelte cup.
+  </p>
+</section>
 <main>
   <ChoroplethMap {datasets} {colors} />
 </main>
@@ -21,5 +31,17 @@
     font-family: "Helvetica";
     display: flex;
     justify-content: center;
+  }
+
+  section {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 0 1rem;
+    font-family: "Helvetica";
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
   }
 </style>
